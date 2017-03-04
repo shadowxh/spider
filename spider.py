9b2807e14f3921d9009a86ae067561860a846519
shadowxh <1343023812@qq.com>
2017-03-05 03:18:53 +0800

    grab contents of the first 30 articles

--- spider.py
+++ spider.py
@@ -12,6 +12,10 @@ headers = {
 def tag_text_is_dianzan(tag):
 	return ((tag.name=='span') and (tag.string==u'点赞'));
 
+def is_chinese(uchar):
+	if uchar>=u'\u4e00' and uchar<=u'\u9fa5':return True;
+	else:return False;
+
 urlname='http://chuansong.me/account/';
 public_names=['thefair2','mimeng7','AspirinMuseum'];
 for public_name in public_names:
@@ -52,16 +56,25 @@ for public_name in public_names:
 				zan_cnt="-1";
 			else:
 				zan_cnt=zan_cnt[0].next_sibling.next_sibling.contents[0].string;
-			articles_all.append((article.string.encode('utf-8'),int(zan_cnt)));
+			articles_all.append((article.string,"http://chuansong.me"+article['href'],int(zan_cnt)));
 			count+=1;
 			print count,zan_cnt;
 
-	articles_all.sort(key=lambda article:article[1],reverse=True);
-	tmp=[];
-	for i in articles_all:
-		tmp.append(i[0][1:]+":"+str(i[1]));
-
+	articles_all.sort(key=lambda article:article[2],reverse=True);
 	print "total:"+str(len(articles_all));
-	_file=open(public_name+'_articles.txt','w');
-	_file.writelines(tmp);
+	for i in range(0,30):#grab the contents of the first 30 articles
+		if i>=len(articles_all):break;
+		print "the ",i+1,"th article";
+		while True:
+                	r=requests.get(url=articles_all[i][1],headers=headers);
+                        r.encoding='utf-8';
+                        if r.ok==True:break;
+             	soup=BeautifulSoup(r.text,'lxml');
+		content=soup.find_all(id='img-content')[0];
+		_file=open(public_name+'.txt','a+');
+		lines=[];
+		for line in content.strings:
+			lines.append(line.encode('utf-8'));
+		_file.writelines(lines);
+		
 	_file.close();
