ccb83120a2368cfa6164423644dfc84d65451aaa
shadowxh <1343023812@qq.com>
2017-03-13 16:27:30 +0800

    sdsd

--- remove_useless_text.py
+++ remove_useless_text.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python  
 #-*- coding:utf-8 -*-
-_file=open(,'a+');
-_file.close();
+import os
+import os.path
+rootdir="./article_content";
+for parent,dirs,files in os.walk(rootdir):
+        for file_name in files:
+                print file_name;
+                if file_name[-4:]!=".txt":continue;
+                _file=open(os.path.join(parent,file_name),"r");
+                articles=[];
+                for line in _file:
+                        if len(line)<=200:continue;
+                        articles.append(line);
+                _file.close();
+                _file=open(os.path.join(parent,file_name[:-4]+"_new.txt"),"a+");
+		_file.writelines(articles);
+		_file.close();
